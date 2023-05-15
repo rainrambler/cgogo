d4de542a3a9a1f5a9e05e6f297f2eca532c87612
rainrambler <wanganyu@outlook.com>
2023-05-15 22:12:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,45 +3,56 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func main() {
 	//fmt.Println("Hello World!")
 	//AppendComma(`D:\tmp\AST\c2v-master\src\aaa.txt`)
-	if os.args.len < 2 {
+	if len(os.Args) < 2 {
 		eprintln("Usage:")
 		eprintln("  c2v file.c")
 		eprintln("  c2v wrapper file.h")
 		eprintln("  c2v folder/")
-		exit(1)
+		return
 	}
-	vprintln(os.args.str())
-	is_wrapper := os.args[1] == "wrapper"
-	path := os.args.last()
-	if !os.exists(path) {
-		eprintln(`"${path}" does not exist`)
-		exit(1)
-	}
-	c2v := new_c2v(os.args)
-	println("C to V translator ${version}")
-	c2v.translation_start_ticks = time.ticks()
-	if os.is_dir(path) {
-		os.chdir(path)
-		println(`"${path}" is a directory, processing all C files in it recursively...\n`)
-		files := os.walk_ext(".", ".c")
 
-		if is_wrapper {
-		} else {
-			if files.len > 0 {
-				for _, file := range files {
-					c2v.translate_file(file)
-				}
-				c2v.save_globals()
+	path := os.Args[len(os.Args)-1] // last
+	if _, err := os.Stat(path); err == nil {
+		//fmt.Printf("File exists\n");
+	} else {
+		fmt.Printf(`"%s" does not exist\n`, path)
+		return
+	}
+
+	c2v := new_c2v(os.Args)
+	println("C to V translator ${version}")
+	c2v.translation_start_ticks = time.Now().UnixMicro()
+
+	fi, _ := os.Stat(path)
+	if fi.IsDir() {
+		files, err := ioutil.ReadDir(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			if is_c_file(f.Name()) {
+				c2v.translate_file(f.Name())
 			}
 		}
+		c2v.save_globals()
 	} else {
 		c2v.translate_file(path)
 	}
-	delta_ticks := time.ticks() - c2v.translation_start_ticks
-	println("Translated ${c2v.translations:3} files in ${delta_ticks:5} ms.")
+	delta_ticks := time.Now().UnixMicro() - c2v.translation_start_ticks
+	fmt.Printf("Translated %v files in %v ms.\n", c2v.translations, delta_ticks)
+}
+
+func is_c_file(filename string) bool {
+	return filepath.Ext(filename) == ".c"
 }
