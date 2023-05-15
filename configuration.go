cd08fd2d8378b155d7a525e525b1b1b3182eda22
rainrambler <wanganyu@outlook.com>
2023-05-15 19:07:44 +0800

    Create configuration.go

---
+++ configuration.go
@@ -0,0 +1,14 @@
+package main
+
+// called once per each .c file
+func (c2v *C2V) set_config_overrides_for_file(path0 string) {
+	// TODO impl
+}
+
+func get_sdl_cflags() string {
+	return ""
+}
+
+func (c2v *C2V) get_additional_flags(path0 string) string {
+	return ""
+}
